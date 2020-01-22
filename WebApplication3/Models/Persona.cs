aed4f2b05519ebdfb8fe0098e7b4126217b18038
snakeice45 <jonas171192@gmail.com>
2020-01-21 20:36:09 -0600

    .NET simple CRUD

---
+++ WebApplication3/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Persona
+    {
+        [Key]
+        public int IdPersona { get; set; }
+        public string NamePersona { get; set; }
+        public string AddressPersona { get; set; }
+    }
+}
